3bf4e92d418531394bf755a201f33b8dd8913d12
friedchicken099-cell <friedchicken099@gmail.com>
2026-02-23 14:42:49 -0500

    Setup Firestore syncing

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,17 @@
-// src/firebase.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCgaF2MkKF4yj2fWLI1JOBFEIk1mWUF8GM",
   authDomain: "vibe-todo-f04e2.firebaseapp.com",
   projectId: "vibe-todo-f04e2",
-  storageBucket: "vibe-todo-f04e2.appspot.com",
+  storageBucket: "vibe-todo-f04e2.firebasestorage.app",
   messagingSenderId: "953396332854",
   appId: "1:953396332854:web:3870e60a5af029abff0b97"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
